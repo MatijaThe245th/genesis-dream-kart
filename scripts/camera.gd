85a2ca17d9d67f2cf81937b7edf006de67140efb
MatijaThe245th <matija245matakovic@gmail.com>
2025-08-01 13:57:14 +0200

    lerp -> tween (Camera)

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -11,20 +11,23 @@ const FOV_MIN: float = 57.5
 const FOV_BOOST: float = 95.0
 
 const DISTANCE_NORMAL: float = 4.0
-const DISTANCE_MAX: float = 3.8
-const DISTANCE_MIN: float = 4.2
-const DISTANCE_BOOST: float = 4.1
+const DISTANCE_MAX: float = 3.5
+const DISTANCE_MIN: float = 2.0
+const DISTANCE_BOOST: float = 4.5
 
 const OFFSET_NORMAL: float = 3.0
 const OFFSET_DRIFT: float = 5.0
 
 # Dynamic variables
 var speed_factor: float
-var target_fov: float
 var target_offset: float
 var target_camera_distance: float
-var fov_lerp_factor: float
 var distance_lerp_factor: float
+
+var FOVTween: Tween
+var DistanceTween: Tween
+var last_fov: float
+var last_distance: float
 
 
 func _ready():
@@ -39,28 +42,17 @@ func _process(delta):
 	
 	# Handle camera FOV and distance
 	if TestCar.forward_direction == -1 and TestCar.acceleration_input < 0.0:
-		target_fov = FOV_MIN
-		target_camera_distance = DISTANCE_MIN
-		fov_lerp_factor = 5.0 * speed_factor
-		distance_lerp_factor = 5.0 * speed_factor
+		fov_tween(FOV_MIN, 0.6, Tween.EASE_IN, Tween.TRANS_LINEAR)
+		distance_tween(DISTANCE_MIN, 0.6, Tween.EASE_IN, Tween.TRANS_LINEAR)
 	elif TestCar.is_boosting:
-		target_fov = FOV_BOOST
-		target_camera_distance = DISTANCE_BOOST
-		fov_lerp_factor = 10.0
-		distance_lerp_factor = 10.0
+		fov_tween(FOV_BOOST, 0.5, Tween.EASE_IN_OUT, Tween.TRANS_BACK)
+		distance_tween(DISTANCE_BOOST, 0.5, Tween.EASE_IN_OUT, Tween.TRANS_BACK)
 	elif TestCar.forward_direction == 1 and TestCar.acceleration_input > 0.0:
-		target_fov = FOV_MAX
-		target_camera_distance = DISTANCE_MAX
-		fov_lerp_factor = 5.0 * speed_factor
-		distance_lerp_factor = 5.0 * speed_factor
+		fov_tween(FOV_MAX, 0.6, Tween.EASE_IN, Tween.TRANS_LINEAR)
+		distance_tween(DISTANCE_MAX, 0.6, Tween.EASE_IN, Tween.TRANS_LINEAR)
 	else:
-		target_fov = FOV_NORMAL
-		target_camera_distance = DISTANCE_NORMAL
-		fov_lerp_factor = 1.5
-		distance_lerp_factor = 1.5
-	
-	Camera.fov = lerp(Camera.fov, target_fov, fov_lerp_factor * delta)
-	Camera.position.z = lerp(Camera.position.z, target_camera_distance, distance_lerp_factor * delta)
+		fov_tween(FOV_NORMAL, 0.6, Tween.EASE_IN, Tween.TRANS_LINEAR)
+		distance_tween(DISTANCE_NORMAL, 0.6, Tween.EASE_IN, Tween.TRANS_LINEAR)
 	
 	# Handle camera horizontal offset
 	if TestCar.is_drifting:
@@ -77,3 +69,29 @@ func _process(delta):
 		rotation.y = lerp(rotation.y, -PI, 4.0 * delta)
 	else:
 		rotation.y = lerp(rotation.y, 0.0, 4.0 * delta)
+
+
+func fov_tween(target_fov: float, duration: float, ease_type: Tween.EaseType, transition_type: Tween.TransitionType):
+	if target_fov != last_fov:
+		if FOVTween:
+			FOVTween.kill()
+		
+		FOVTween = create_tween()
+		FOVTween.set_ease(ease_type)
+		FOVTween.set_trans(transition_type)
+		FOVTween.tween_property(Camera, "fov", target_fov, duration)
+		
+		last_fov = target_fov
+
+
+func distance_tween(target_distance: float, duration: float, ease_type: Tween.EaseType, transition_type: Tween.TransitionType):
+	if target_distance != last_distance:
+		if DistanceTween:
+			DistanceTween.kill()
+		
+		DistanceTween = create_tween()
+		DistanceTween.set_ease(ease_type)
+		DistanceTween.set_trans(transition_type)
+		DistanceTween.tween_property(Camera, "position:z", target_distance, duration)
+		
+		last_distance = target_distance
